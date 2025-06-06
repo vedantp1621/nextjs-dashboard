4beb22f54322e0b23b505ace777f583a897cfff0
Vedant Patel <vepatel@isaca.org>
2025-06-06 09:26:41 -0500

    init commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { subscribe } from 'diagnostics_channel';
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana( {
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
